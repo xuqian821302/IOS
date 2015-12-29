74b714418dd8eb8c1b3596e9043d9292d23d888c
xuqian <1282622754@qq.cpm>
2015-12-29 15:28:13 +0800

    test git

---
+++ test/test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by spare on 15/12/29.
+//  Copyright © 2015年 tarena.kren. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
